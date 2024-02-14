16b946cdc1b2068ed5d12645bcccd553af0fa1b3
adilet-wd <usenkanov.adilet.kanatbekovich@gmail.com>
2024-02-14 19:00:34 +0600

    commit before pull

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+import { createProxyMiddleware } from 'http-proxy-middleware';
 const nextConfig = {
   env: {
     API_ROUTE: 'http://217.151.230.35:999/api/v1',
@@ -12,6 +13,14 @@ const nextConfig = {
         pathname: '/media/media/**',
       },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/v1/:path*',
+        destination: 'http://217.151.230.35:999/api/v1/:path*',
+      },
+    ];
   },
 };
 
