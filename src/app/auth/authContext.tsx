f7f3402e999e036ea38cf4ce227576b3be1dd7a7
adilet-wd <usenkanov.adilet.kanatbekovich@gmail.com>
2024-02-15 02:10:33 +0600

    Попытка починить авторизацию

--- src/app/auth/authContext.tsx
+++ src/app/auth/authContext.tsx
@@ -14,14 +14,44 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
     return false;
   });
 
+  const [refreshToken, setRefreshToken] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const currentAuth = localStorage.getItem('ProsekgRefreshToken');
+      console.log(`refresh: ${currentAuth}`);
+      return currentAuth ? currentAuth : '';
+    }
+    return '';
+  });
+
+  const [accessToken, setAccessToken] = useState(() => {
+    if (typeof window !== 'undefined') {
+      const currentAuth = localStorage.getItem('ProsekgAccessToken');
+      console.log(`acces: ${currentAuth}`);
+      return currentAuth ? currentAuth : '';
+    }
+    return '';
+  });
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       localStorage.setItem('isAuthenticatedOnProseKG', String(isAuthenticated));
     }
   }, [isAuthenticated]);
 
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('ProsekgRefreshToken', String(refreshToken));
+    }
+  }, [refreshToken]);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('ProsekgAccessToken', String(accessToken));
+    }
+  }, [accessToken]);
+
   return (
-    <AuthContext.Provider value= {{ isAuthenticated, setIsAuthenticated }}>
+    <AuthContext.Provider value= {{ isAuthenticated, setIsAuthenticated, accessToken, setAccessToken, refreshToken, setRefreshToken } as { isAuthenticated: boolean; setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>; accessToken: string; setAccessToken: React.Dispatch<React.SetStateAction<string>>; refreshToken: string; setRefreshToken: React.Dispatch<React.SetStateAction<string>> }}>
       {children}
     </AuthContext.Provider>
   );
--- src/app/myProfile/page.tsx
+++ src/app/myProfile/page.tsx
@@ -5,13 +5,37 @@ import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../auth/authContext";
 import LoadingScreen from "@/components/loadingScreen/loadingScreen";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
-
+interface User {
+  id: number;
+  password: string;
+  last_login: null;
+  link: string;
+  is_superuser: false,
+  first_name: string,
+  last_name: string,
+  is_staff: false,
+  is_active: true,
+  date_joined: string,
+  name: string,
+  surename: string,
+  username: string,
+  email: string,
+  avatar: null,
+  created_at: string,
+  confirmation_code: string,
+  groups: [],
+  user_permissions: []
+}
 
 export default function MyProfile() {
   const authContext = useContext(AuthContext);
   const { isAuthenticated, setIsAuthenticated } = authContext || {};
   const [isClient, setIsClient] = useState(false);
+  const { refreshToken, setRefreshToken } = authContext || {};
+  const { accessToken, setAccessToken } = authContext || {};
+  const { user, setUser} = useState<User>();
   const router = useRouter();
 
   // Проверка на клиента
@@ -24,7 +48,14 @@ export default function MyProfile() {
     if (!isAuthenticated) {
       router.push('/'); 
     }
-}, [isAuthenticated]);
+  }, [isAuthenticated]);
+
+  useEffect(() => {
+    if(isClient && refreshToken) {
+      getProfile();
+    }
+  }, [isClient, refreshToken]);
+
 
   function login() {
     if (setIsAuthenticated) {
@@ -35,6 +66,34 @@ export default function MyProfile() {
   function logout(){
     if (setIsAuthenticated) {
       setIsAuthenticated(false);
+    }
+  }
+
+  async function getProfile() {
+    try {
+      console.log(refreshToken);
+      const newAccessToken = await axios.post(
+        `${process.env.API_ROUTE}/regauth/refresh-token/`,
+        {
+          refresh: refreshToken,
+        }
+      );
+      setAccessToken(newAccessToken.data.access);
+      console.log(newAccessToken.data.access);
+      const res = await axios.post(`${process.env.API_ROUTE}/regauth/user-profile/`, {}, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      }
+      );
+      setUser(res.data);
+
+    } catch (error) {
+      if ((error as any)?.response?.status === 400) {
+        console.log((error as any).response.data);
+      } else {
+        console.log(`Ошибка`, error);
+      }
     }
   }
 
@@ -54,6 +113,7 @@ export default function MyProfile() {
         <Container>
             <h2>Здесь будет страница с личным кабинетом</h2>
             <p>{isAuthenticated ? 'Пользователь вошел' : 'Пользователь не вошел'}</p>
+            <p>{user?.username}</p>
             <button onClick={logout}>Выйти</button>
             <button onClick={login}> Войти</button>
         </Container>
